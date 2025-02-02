5f48d01557a275145ffa0751aecd3671e683b0b6
joabe-nascimento <joabefnascimento1@outlook.com>
2025-02-02 15:47:37 -0300

    foi adicionado o campo photo em models

--- src/models/User.js
+++ src/models/User.js
@@ -1,29 +1,37 @@
 // backend/models/User.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    // Campo para armazenar a foto do perfil (em formato Base64)
+    photo: {
+      type: String,
+      default: null,
     },
     // Campos para redefinição de senha:
     resetPasswordToken: {
-        type: String,
+      type: String,
     },
     resetPasswordExpires: {
-        type: Date,
+      type: Date,
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
